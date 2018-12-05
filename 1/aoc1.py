86ab5f91f6e0fedd4459422e61168779a73ac13b
Monty Secor <monty.secor@gmail.com>
2018-12-05 13:46:38 -0600

    Initial Commit, AoC 2018

---
+++ 1/aoc1.py
@@ -0,0 +1,15 @@
+import os.path
+import ctypes
+
+# Starting frequency of 0; will accumulate all input lines for net frequency change
+freqSum = "0"
+workingDir = "G:/Python/adventofcode2018/1/"
+
+# Each line of the file is a string beginning with + or -, followed by an integer
+inputFile = open(workingDir + "input")
+for line in inputFile:
+        freqSum = freqSum + line.rstrip()                # Concatenate all lines, remove newline
+
+# Once all numbers with +/- signs are appended, evaluate to get the total:
+print(eval(freqSum))
+#ctypes.windll.user32.MessageBoxW(0, "Frequency result: " + eval(freqSum), "Frequency Shift", 0)
