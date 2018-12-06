ba1ccb912e4a51b3ab49d1f75c1418290df4c568
Monty Secor <monty.secor@gmail.com>
2018-12-06 14:12:00 -0600

    Initial design of Day2 program

---
+++ 2/aoc2.py
@@ -0,0 +1,32 @@
+from difflib import SequenceMatcher
+from collections import Counter, defaultdict
+
+# Need to find IDs with exactly 2 or 3 counts of the any one letter.
+workingDir = "G:/Python/adventofcode2018/2/"
+twoChars, threeChars = 0, 0
+boxIds = defaultdict(str)
+
+# Read each box ID into a defaultdict
+with open(workingDir + "input2") as inFile:
+    boxIds = [line.strip() for line in inFile]
+    inFile.close()
+
+# Part 1:
+# Find all box IDs that contain exactly 2 or exactly 3 of any one character.
+# Then, compute a checksum, of the number of 2-count IDs multiplied by the number of 3-count IDs.
+for box in boxIds:
+    found2 = False
+    found3 = False
+    charCount = Counter(box)
+    print("Box ID: ", box, "\t", charCount)
+
+    for letter in charCount:
+
+    if found2:
+        twoChars += 1
+    if found3:
+        threeChars += 1
+
+print("\n\nBoxes with exactly 2 duplicate chars: ", twoChars)
+print("Boxes with exactly 3 duplicate chars: ", threeChars)
+print("\nChecksum : ", twoChars * threeChars)
