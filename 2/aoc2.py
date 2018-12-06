7b699d884577b09ee0a7ff5ee6b26eee49fc97d0
Monty Secor <monty.secor@gmail.com>
2018-12-06 16:09:32 -0600

    Complete part 1 and 2 solutions

--- 2/aoc2.py
+++ 2/aoc2.py
@@ -1,5 +1,5 @@
-from difflib import SequenceMatcher
 from collections import Counter, defaultdict
+from itertools import combinations
 
 # Need to find IDs with exactly 2 or 3 counts of the any one letter.
 workingDir = "G:/Python/adventofcode2018/2/"
@@ -34,3 +34,27 @@ for box in boxIds:
 print("\n\nBoxes with exactly 2 duplicate chars: ", twoChars)
 print("Boxes with exactly 3 duplicate chars: ", threeChars)
 print("\nChecksum : ", twoChars * threeChars)
+
+
+# Part 2:
+# Box IDs are strings of 26 characters long. Need to find two box IDs that differ by only 1 character.
+# E.G., 25 of 26 characters should be in the same position.
+# The answer to submit will be the characters that the two Box IDs have in common.
+
+# Use itertools combinations to get all possible pairs of box IDs
+for box1, box2 in combinations(boxIds, 2):
+    #print(box1, ",", box2)
+
+    # Box IDs should be same length, but just in case, we'll only match across the minimum length
+    #minLen = min(len(box1), len(box2))
+    # If common chars are 1 less than total length of boxID, we have a match!
+    #commonChars = [i for i,j in zip(box1,box2) if i == j]
+    commonChars = list()
+    for i,j in zip(box1,box2):
+        if i == j:
+            commonChars.append(i)
+    if len(box1) - len(commonChars) == 1:
+        print("Found box IDs:\n", box1, "\n", box2)
+        print("Common characters:", "".join(commonChars))
+        break
+
