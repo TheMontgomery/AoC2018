09293a851cfdeb18fae10adcfec73a934c3191c5
Monty Secor <monty.secor@gmail.com>
2018-12-06 21:52:23 -0600

    Working solution for day 3, parts 1 and 2 (read file 2x)

--- 3/aoc3.py
+++ 3/aoc3.py
@@ -1,8 +1,12 @@
 import re
-from collections import defaultdict
 
-# Day 3 Advent of Code 2018 Puzzle
+# Defines the total cloth, 1000x1000 inches
+CLOTHSIZE = 1000
+cloth = []
+for _ in range(CLOTHSIZE):
+    cloth.append([0 for _ in range(CLOTHSIZE)])
 
+workingDir = "G:/Python/adventofcode2018/3/"
 # ------ PART 1 ------
 # Input provides a list of claims for fabric (1000 x 1000 inches)
 # Need to find total area of overlapping claims
@@ -10,12 +14,7 @@ from collections import defaultdict
 #26 @ 594,575: 24x15
 #id @ xPos,yPos: lenXheight
 
-# Defines the total cloth, 1000x1000 inches
-cloth = []
-for _ in range(1000):
-    cloth.append([0 for _ in range(1000)])
 
-workingDir = "G:/Python/adventofcode2018/3/"
 # Read each line of the file, parse into integers.
 # Input:    line (string)
 # Returns:  5-element Tuple of Integers: (id, x, y, width, height)
@@ -40,7 +39,6 @@ for claim in claims:
         for j in range (y, y + height):
             cloth[i][j] += 1
 
-
 # Finally, iterate over the global cloth, and sum any coordinates with more than 1 claim
 # for row in cloth:
 #     for col in row:
@@ -52,5 +50,24 @@ for claim in claims:
 overlap = [col for row in cloth for col in row if col > 1]
 print("Total area of overlap: ", len(overlap))
 
-
 # ----- PART 2 -----
+# From the claims in the input, we are given that only ONE claim does not overlap any others
+# Need to identify it with the claimId somehow
+with open(workingDir + "input") as inFile:
+    #claims = [line.strip() for line in inFile]
+    claims = []
+    for record in inFile:
+        claims.append(parseLine(record.strip()))
+    inFile.close()
+
+for claim in claims:
+    #print(list(claim))
+    claimId, x, y, width, height = claim
+    noOverlap = True
+    for i in range(x, x + width):
+        for j in range(y, y + height):
+            if cloth[i][j] > 1:
+                noOverlap = False
+
+    if noOverlap:
+        print("ClaimId without overlap: ", claimId)
