76f3e3defef18a1b19e8238bac9c634a48ffbb98
Monty Secor <monty.secor@gmail.com>
2018-12-07 21:13:55 -0600

    Semi-working, correctly tabulates time slept

--- 4/aoc4.py
+++ 4/aoc4.py
@@ -1,8 +1,9 @@
-import datetime
-import re
+from datetime import datetime
 from collections import defaultdict
 
 workingDir = "G:/Python/adventofcode2018/4/"
+dateFormat = "%Y-%m-%d %H:%M"
+
 
 # ----- PART 1 -----
 # Input file is an unsorted list of date/timestamps, each logging an action (guard starts shift, falls asleep, wakes up)
@@ -13,20 +14,35 @@ workingDir = "G:/Python/adventofcode2018/4/"
 
 # Takes an input file, reads it into a list, and then sorts by date.
 # Returns sorted list
-def sort_date_time(file_path):
+def sortDateTime(file_path):
     with open(file_path) as infile:
-        log_entries = []
-        date_pattern = r'(\d\d\d\d)-(0?[0-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])'
-        for record in infile:
-
-            timestamp, _, log_event = record.partition("] ")
-            log_datetime = datetime.datetime()
-            print(timestamp, " ", log_event)
-            #timestamp =
-            #year, month, day, hour, minute = re.compile(date_pattern).match(record).groups()
-            #print("year: ", year, " month: ", month, " day: ", day, " hour: ", hour, " minute: ", minute)
-            #print(record)
-            #print("Date: ", log_date, " Time: ", log_time, " Detail: ", log_event)
+        log_entries = infile.read().splitlines()
+        log_entries.sort()
+        # print(log_entries)
+        return log_entries
 
 
-sort_date_time(workingDir + "input2")
+# Record the frequency of each minute in the midnight hour asleep, indexed by guard ID
+guards = defaultdict(list)
+# Record each guard's total minutes asleep, indexed by guard ID
+timeAsleep = defaultdict(int)
+
+records = sortDateTime(workingDir + "input2")
+for record in records:
+    timestamp, guardAction = record.split("] ")
+
+    if "Guard #" in guardAction:
+        # Parse guard ID, add to dictionary
+        guardId = int(guardAction.split()[1].replace("#", ""))
+        print("guardID: ", guardId)
+    elif "falls asleep" in guardAction:
+        # Get start time for sleep
+        sleepStart = datetime.strptime(timestamp.replace("[", ""), dateFormat)
+        print("Guard sleep start:", sleepStart.minute)
+    elif "wakes up" in guardAction:
+        # Get end time for sleep, record total minutes asleep
+        sleepEnd = datetime.strptime(timestamp.replace("[", ""), dateFormat)
+        print("Guard sleep end:", sleepEnd.minute)
+        timeAsleep[guardId] += sleepEnd.minute - sleepStart.minute
+        print("Guard #", guardId, "total time asleep: ", timeAsleep[guardId])
+        guards[guardId].append((sleepStart.minute, sleepEnd.minute))
