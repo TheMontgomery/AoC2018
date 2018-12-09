5066bf603ea2e813701114b75c02031a4b7e05f0
Monty Secor <monty.secor@gmail.com>
2018-12-08 21:56:17 -0600

    Solution for parts 1 and 2

--- 5/aoc5.py
+++ 5/aoc5.py
@@ -9,12 +9,10 @@ workingDir = "G:/Python/adventofcode2018/5/"
 
 # ----- PART 1 -----
 # After all possible reactions, find total length of polymer.
-with open(workingDir + "input3") as inFile:
-    rawPolymer = inFile.read()
+with open(workingDir + "input") as inFile:
+    rawPolymer = inFile.read().strip("\n")
     inFile.close()
 
-print(rawPolymer)
-print(len(rawPolymer))
 
 def opposite(a, b):
     return (a.lower() == b.lower() and a.swapcase() == b)
@@ -29,15 +27,13 @@ def polReduce(polymer):
             buffer.append(element)              # Add each character
         #print(i, element, polymer[i-1])
         elif i > 0:                             # If previous character exists...
-            #print(element, polymer[i-1], buffer[-1])
-            if opposite(element, buffer[-1]):   # If polarities oppose, remove previous char, and continue
+            #print(element, len(buffer), buffer[-1])
+            if len(buffer) and opposite(element, buffer[-1]):   # If opposite, remove previous char & don't append new
                 #print("Opposite detected")
                 buffer.pop()
             else:
                 buffer.append(element)
-        #print(buffer)
 
-    #print(''.join(buffer))
     return ''.join(buffer)
 
 reduced = polReduce(rawPolymer)
@@ -45,3 +41,23 @@ reduced = polReduce(rawPolymer)
 print("Length of original:", len(rawPolymer))
 print("Reduced polymer:", reduced)
 print("Reduced length:", len(reduced))
+
+
+# ----- PART 2 -----
+# Need to find which element type can be removed to produce the minimum reduced polymer size
+
+unitreduce = defaultdict(int)
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+def polReduceUnit(polymer, letter):
+    p = polymer.replace(letter.lower(), '')
+    p = p.replace(letter.upper(), '')
+    return polReduce(p)
+
+for letter in alpha:
+    reduced = polReduceUnit(rawPolymer, letter)
+    #print("letter:", letter, "\tlength:", len(reduced), "\treduced:", reduced)
+    unitreduce[letter] = len(reduced)
+
+print("Optimal letter:", min(unitreduce.items(), key=lambda i: i[1]))
+
