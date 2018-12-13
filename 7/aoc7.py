38ced38bd9425209d1c7e7b67eb4168f2fc9f7ab
Monty Secor <monty.secor@gmail.com>
2018-12-12 19:52:32 -0600

    Part 1 complete

--- 7/aoc7.py
+++ 7/aoc7.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 workingDir = "G:/Python/adventofcode2018/7/"
 
-steps = defaultdict(set)
+steps = set()
 prereqs = defaultdict(set)
 stepOrder = ''
 # Read the steps, and extract the prerequisite step, along with the step.
@@ -10,40 +10,39 @@ stepOrder = ''
 # Step C must be finished before step A can begin.
 #
 # Where "C" is the prerequisite, and A is the next step
-with open(workingDir + 'input2') as inFile:
+# Some steps will not have prerequisites, hence need to have master set of all steps
+with open(workingDir + 'input') as inFile:
     for line in inFile:
         prereq, step = line.rstrip().split(" ")[1:9:6]
-        steps[prereq].add(step)
+        steps.add(step)
+        steps.add(prereq)
         prereqs[step].add(prereq)
     inFile.close()
 
 
 print("prereqs:", prereqs)
 print("steps:", steps)
-# InOrder traversal of the list of steps, defaultdict should mimic Binary Search Tree
-# Accepts List argument, named subtree
-def inOrder(subtree):
-    stack = []
-    if len(subtree) == 1:
-        stack.append(subtree[0])
-    else:
-        stack = inOrder(subtree[1:])
+print("len steps:", len(steps))
+print("len prereqs:", len(prereqs))
 
-for step in steps:
-    print(step, sorted(steps[step]))
-    # Get list of all child steps
-    children = sorted(steps[step])
+# Convert set of steps to sorted list, as we need to evaluate alphabetically
+steps = sorted(steps)
+print(steps)
+while steps:
+    for step in steps:
+        parents = sorted(prereqs[step])
+        # print("\nstep:", step, "parents:", parents)
+        # print("len(parents):", len(parents))
+        # print("len(stepOrder):", len(stepOrder))
 
-    # If the current step is not in the order, add it
-    if step not in stepOrder:
-        stepOrder.append(step)
+        # Check if parents is empty, OR if ALL parents are already complete
+        step_complete = (len(parents) == 0) or all(prereq in stepOrder for prereq in parents)
+        # print("step_complete:", step_complete)
 
-    # If current step has only 1 child, append the child step; else, do breadth-first search
-    if len(children) == 1:
-        stepOrder.append(children[0])
-    elif len(children) > 1:
-        stepOrder.pop()
-        print(inOrder(children))
+        if step_complete:
+            stepOrder += step
+            steps.remove(step)
+            break
 
-print("Part 1:", stepOrder)
+print("Part 1:", stepOrder, steps)
 
