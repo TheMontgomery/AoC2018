91ba42bf387618cc62b4d28de8ad86813517e817
Monty Secor <monty.secor@gmail.com>
2018-12-14 21:17:57 -0600

    2nd commit, semi-functional

--- 8/aoc8.py
+++ 8/aoc8.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from string import ascii_uppercase as alpha
 #import networkx as nx
 
 workingDir = "G:/Python/adventofcode2018/8/"
@@ -10,30 +11,36 @@ with open(workingDir + 'input2') as inFile:
     inFile.close()
 
 print(master)
-meta = defaultdict(list)
-children = defaultdict(list)
-payloads = defaultdict(list)
-i = 0
 
 # Function to parse the "tree"
 # Input: List, of integers (a subset of the master list)
 # Returns: Sum of metadata, value of metadata
 def getData(inputList):
     numChildren, numMeta = inputList[:HEADER_LENGTH]
+    sumMeta = 0
 
-    print("child nodes: {0} metadata entries: {1}".format(numChildren, numMeta))
+    print("\nEnter getData. numChildren: {0}\tnumMeta:{1}".format(numChildren, numMeta))
+    print("inputList:", inputList)
 
-    metadata = inputList[-numMeta:]
-    print("metadata:", metadata)
-    print("length of header + metadata:", len(inputList[:2] + metadata))
-    payload = inputList[2:-numMeta]
-    print("payload:", payload)
+    if numChildren > 0:
+        payload = inputList[HEADER_LENGTH:-numMeta]
+        print("payload:", payload)
+        for n in range(numChildren):
+            #print(getData(payload))
+            sumMeta += getData(payload)
+    elif numChildren == 0:
+        print("no child nodes. Sum of metadata:", sum(inputList[-numMeta:]))
+        sumMeta += sum(inputList[-numMeta:])
+    # print("\nchild nodes: {0} metadata entries: {1}".format(numChildren, numMeta))
+    # print("metadata:", metadata)
+    # print("length of header + metadata:", len(inputList[:HEADER_LENGTH] + metadata))
+    # payload = inputList[HEADER_LENGTH:-numMeta]
+    # print("payload:", payload)
 
-    if numChildren == 0:
-        return sum(metadata)
-    # Evaluate each child
-    for child in range(numChildren):
-        return getData(payload)
+    return sumMeta
 
+sumMeta = getData(master)
 
-getData(master)
+# ----- PART 1 ------
+# Display the sum of all metadata entries.
+print("Sum of metadata:", sumMeta)
